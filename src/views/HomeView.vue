624903a97bd5892dfe230213734dbe9c91be1937
Bazga2k1 <filippo.bubic@gmail.com>
2023-07-01 22:36:21 +0200

    Početak implementacije početne stranice

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Inventory</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+  
+      <main>
+        <section class="banner">
+          <h1>Audi</h1>
+          <p>Vorsprung durch Technik</p>
+          <a href="#" class="btn">Browse Inventory</a>
+        </section>
+  
+        <section class="featured-cars">
+          <h2>Featured Cars</h2>
+          <div v-for="car in cars" :key="car.id" class="car-card">
+            <img :src="car.image" :alt="car.model">
+            <h3>{{ car.model }}</h3>
+            <p>Price: {{ car.price }}</p>
+            <a href="#" class="btn">View Details</a>
+          </div>
+        </section>
+      </main>
+  
+      <footer>
+        <p></p>
+      </footer>
+    </div>
+  </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    cars() {
+        return {
+          featuredCars: [
+            {
+              id: 1,
+              model: "Audi A1",
+              price: "29,49.50€",
+              image: "car1.jpg",
+            },
+            {
+              id: 2,
+              model: "Audi A3",
+              price: "42,780.90€",
+              image: "car2.jpg",
+            }]
+            };
+        }
+    }
 }
+
 </script>
+
+<style scoped>
+</style>
