7e857c8c16d655e92da4748851eeefe4ddf80550
Bazga2k1 <filippo.bubic@gmail.com>
2023-08-16 17:49:33 +0200

    Update konfiguracije 3
    - dopunjen katalog naplataka za 17" i 18"

--- src/views/ConfigView1.vue
+++ src/views/ConfigView1.vue
@@ -1,6 +1,8 @@
 <template>
   <v-container>
     <h1>Odaberite paket naplataka</h1><br>
+    <v-divider></v-divider>
+
     <v-card-title>17"</v-card-title>
     <v-row>
         <v-col>
@@ -37,19 +39,54 @@
         </v-col>
       <br>
     </v-row>
-    <v-row>
-      <v-card-title>18"</v-card-title>
+    <v-divider></v-divider>
 
+    <v-card-title>18"</v-card-title>
+    <v-row>
+      <v-col>
+        <rimsCardComponent
+          rimName="Audi Sport aluminijski naplatak sa sjajnim crnim detaljima"
+          velicina="7.5J x 18"
+          cijena="2500"
+          image-url="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE897ld7-urnCrdEKSUNWN6l75z0-mPDMwMDBwMIAwyhUFQw4BIIMzu4hri6OkTDADE77yEuAAAAA?wid=90">
+      </rimsCardComponent>
+      </v-col>
+      <v-col>
+        <rimsCardComponent
+          rimName="Y aluminijski brušeni petokraki naplatak"
+          velicina="7.5J x 18"
+          cijena="1200"
+          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8ksf3Pf7WmJXVc1E3-n3IvgDx-zK1DMwMDBwMIAwyhUFQw4BIIMzu4hri6OkTDACx2bZHuAAAAA?wid=90">
+      </rimsCardComponent>
+      </v-col>
+      <v-col>
+        <rimsCardComponent
+          rimName="Audi Sport sjajni crni zvjezdani aluminijski naplatak"
+          velicina="7.5J x 18"
+          cijena="2800"
+          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE80rtap3cZ7fzYMMXLRn7STaaFEb16DMwMDBwMIAwyhUFQw4BIIMzu4hri6OkTDAD-iEAjuAAAAA?wid=90">
+      </rimsCardComponent>
+      </v-col>
+      <v-col>
+        <rimsCardComponent
+          rimName="Audi Sport dvostruki aluminijski naplatak"
+          velicina="7.5J x 18"
+          cijena="2800"
+          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8X4xK9mt6FW6bsOrYVKHUnOn-Cx_cYGBmYOBgAGGQKQyCGgZEAmF2F9cQR0-fYACMyCNkuAAAAA?wid=90">
+      </rimsCardComponent>
+      </v-col>
       <br>
     </v-row>
-    <v-row>
-      <v-card-title>19"</v-card-title>
-
-      <br>
-    </v-row>
-    <v-row>
-      <v-card-title>20"</v-card-title>
+    <v-divider></v-divider>
     
+    <v-card-title>19"</v-card-title>
+    <v-row>
+      <br>
+    </v-row>
+    <v-divider></v-divider>
+
+    <v-card-title>20"</v-card-title>
+    <v-row>   
       <br>
     </v-row>
 
