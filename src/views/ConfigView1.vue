c9447a235c158e355bcc879c4b3d2c732ec54d00
Bazga2k1 <filippo.bubic@gmail.com>
2024-09-02 12:00:26 +0200

    Update home, konfiguracija
    - prikaz početne stranice s inventarom automobila iz baze podataka
    - prikaz stranice konfiguracije naplataka sa inventarom iz baze podataka

--- src/views/ConfigView1.vue
+++ src/views/ConfigView1.vue
@@ -3,173 +3,116 @@
     <h1>Odaberite paket naplataka</h1><br>
     <v-divider></v-divider>
 
-    <v-card-title>17"</v-card-title>
-    <v-row class="my-2">
-        <v-col>
-          <rimsCardComponent
-          rimName="Aluminijski petokraki naplaci"
-          velicina="7.5j x 17"
-          cijenaR="0"
-          image-url="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8XxKV3hc_3ctXtM5wjtOcx-F2Gk0XGZgZGDgYQBhkCoOghgGRQJjdxTXE0dMnGADd4GG8uAAAAA?wid=90">
-          </rimsCardComponent>
-        </v-col>
-        <v-col>
-          <rimsCardComponent
-            rimName="Aluminijski petokraki naplaci sa plastičnim umecima"
-            velicina="7.5j x 17"
-            cijenaR="300"
-            image-url="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8S0OWM9zlDOWu21O35uEjqX0bco0vMjAzMHAwgDDIFAZBDQMigTC7i2uIo6dPMADF_LxXuAAAAA?wid=90">
-          </rimsCardComponent>
-        </v-col>
-        <v-col>
-          <rimsCardComponent
-            rimName="Aluminijski petokraki naplaci sa crnim plastičnim umecima"
-            velicina="7.5j x 17"
-            cijenaR="300"
-            image-url="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE83xzjpsm0XtivuS_uWOhG5pnfv86rZmBmYOBgAGGQKQyCGgZEAmF2F9cQR0-fYADbkBSeuAAAAA?wid=90">
-          </rimsCardComponent>
-        </v-col>
-        <v-col>
-          <rimsCardComponent
-            rimName="Audi Sport matirani aluminijski naplaci - titanij"
-            velicina="7.5J x 17"
-            cijenaR="1500"
-            image-url="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE85nN6fpgtXuzjvaiO9-Cniy_5toZIMzAzMHAwgDDIFAZBDQMigTC7i2uIo6dPMABV2zOJuAAAAA?wid=90">
-          </rimsCardComponent>
-        </v-col>
-      <br><br>
-    </v-row>
-    <v-divider></v-divider>
+    <div v-if="loading" class="loading">Loading rims...</div>
+    <div v-if="error" class="error">{{ error }}</div>
 
-    <v-card-title>18"</v-card-title>
-    <v-row class="my-2">
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport aluminijski naplaci sa sjajnim crnim detaljima"
-          velicina="7.5J x 18"
-          cijenaR="2500"
-          image-url="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE897ld7-urnCrdEKSUNWN6l75z0-mPDMwMDBwMIAwyhUFQw4BIIMzu4hri6OkTDADE77yEuAAAAA?wid=90">
-      </rimsCardComponent>
-      </v-col>
-      <v-col>
-        <rimsCardComponent
-          rimName="Y aluminijski brušeni petokraki naplaci"
-          velicina="7.5J x 18"
-          cijenaR="1200"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8ksf3Pf7WmJXVc1E3-n3IvgDx-zK1DMwMDBwMIAwyhUFQw4BIIMzu4hri6OkTDACx2bZHuAAAAA?wid=90">
-      </rimsCardComponent>
-      </v-col>
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport sjajni crni zvjezdani aluminijski naplaci"
-          velicina="7.5J x 18"
-          cijenaR="2800"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE80rtap3cZ7fzYMMXLRn7STaaFEb16DMwMDBwMIAwyhUFQw4BIIMzu4hri6OkTDAD-iEAjuAAAAA?wid=90">
-      </rimsCardComponent>
-      </v-col>
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport dvostruki aluminijski naplaci"
-          velicina="7.5J x 18"
-          cijenaR="2800"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8X4xK9mt6FW6bsOrYVKHUnOn-Cx_cYGBmYOBgAGGQKQyCGgZEAmF2F9cQR0-fYACMyCNkuAAAAA?wid=90">
-      </rimsCardComponent>
-      </v-col>
-      <br><br>
-    </v-row>
-    <v-divider></v-divider>
-    
-    <v-card-title>19"</v-card-title>
-    <v-row class="my-2">
-      <v-col>
-        <rimsCardComponent
-          rimName="Dvostruki S-stil petokraki naplatci (Grafit siva)"
-          velicina="8.5J x 19"
-          cijenaR="0"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8pw5y-b2a8r9--T9dRs0lNyJYpQSPMzAzMHAwgDDIFAZBDQMigTC7i2uIo6dPMADl8Vl2uAAAAA?wid=90">
-        </rimsCardComponent>
-      </v-col>
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport petokraki 'Pylon' naplatci"
-          velicina="8.5J x 19"
-          cijenaR="1000"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8_K0i7WoJB8T3HvFobY7TYBKavOAZAzMDAwcDCINMYRDUMCASCLO7uIY4evoEAwAJZuvVuAAAAA?wid=90">
-        </rimsCardComponent>
-      </v-col>
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport V-style"
-          velicina="8.5J x 19"
-          cijenaR="400"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8WzbHRE4Xrj5_bfkehtjISKP98_sPMjAzMHAwgDDIFAZBDQMigTC7i2uIo6dPMAAYaP4wuAAAAA?wid=90">
-        </rimsCardComponent>
-      </v-col>
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport 10-kraki naplaci stila zvjezde"
-          velicina="8.5J x 19"
-          cijenaR="1250"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8WzbHRE4Xrj5_bfkehtjISKP98_sPMjAzMHAwgDDIFAZBDQMigTC7i2uIo6dPMAAYaP4wuAAAAA?wid=90">
-        </rimsCardComponent>
-      </v-col>
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport dvostruki petokraki naplaci"
-          velicina="8.5J x 19"
-          cijenaR="1500"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8mXza_QY_5DY8f3h0A4dqff2a0DxJBmYGBg4GEAaZwiCoYUAkEGZ3cQ1x9PQJBgCSjCAeuAAAAA?wid=90">
-        </rimsCardComponent>
-      </v-col>
-      <br><br>
-    </v-row>
-    <v-divider></v-divider>
+    <template v-if="rims.length > 0">
+      <v-card-title v-if="rims.some(rim => rim.rimSize.startsWith('7.5j x 17'))">17"</v-card-title>
+      <v-row class="my-2" v-if="rims.some(rim => rim.rimSize.startsWith('7.5j x 17'))">
+        <v-col v-for="rim in rims.filter(rim => rim.rimSize.startsWith('7.5j x 17'))" :key="rim._id">
+          <rimsCardComponent
+            :rimName="rim.rimName"
+            :velicina="rim.rimSize"
+            :cijenaR="rim.priceR"
+            :image-url="rim.rimImageUrl"
+          />
+        </v-col>
+      </v-row>
+      <v-divider v-if="rims.some(rim => rim.rimSize.startsWith('7.5j x 17'))"></v-divider>
 
-    <v-card-title>20"</v-card-title>
-    <v-row class="my-2">
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport dvostruki poligonalni petokraki naplaci"
-          velicina="9.0J x 20"
-          cijenaR="3000"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8a0I_7188SYL1U8u6rtk3uj46_FI_xMDMwMDBAMIgUxgENQyIBMLsLq4hjp4-wQCxA0QMuAAAAA?wid=90">
-        </rimsCardComponent>
-      </v-col>
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport petokraki rotorni naplaci"
-          velicina="9.0J x 20"
-          cijenaR="3000"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8a0I_7188SYL1U8u6rtk3uj46_FI_xMDMwMDBAMIgUxgENQyIBMLsLq4hjp4-wQCxA0QMuAAAAA?wid=90">
-        </rimsCardComponent>
-      </v-col>
-      <v-col>
-        <rimsCardComponent
-          rimName="Audi Sport V-style zvjezdani petokraki naplaci"
-          velicina="9.0J x 20"
-          cijenaR="3000"
-          imageUrl="https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEWxlYS0qLkvIZgewAJq7M3MT0VP2sgtR0fka-gqL8lNLkkpTUksTMnGJ2kCE8a75If-wX3uAmYCrrfaf8uLlwtW4TAzMDAwcDCINMYRDUMCASCLO7uIY4evoEAwBHItmIuAAAAA?wid=90">
-        </rimsCardComponent>
-      </v-col>
-      <br><br>
-    </v-row>
+      <v-card-title v-if="rims.some(rim => rim.rimSize.startsWith('7.5j x 18'))">18"</v-card-title>
+      <v-row class="my-2" v-if="rims.some(rim => rim.rimSize.startsWith('7.5j x 18'))">
+        <v-col v-for="rim in rims.filter(rim => rim.rimSize.startsWith('7.5j x 18'))" :key="rim._id">
+          <rimsCardComponent
+            :rimName="rim.rimName"
+            :velicina="rim.rimSize"
+            :cijenaR="rim.priceR"
+            :image-url="rim.rimImageUrl"
+          />
+        </v-col>
+      </v-row>
+      <v-divider v-if="rims.some(rim => rim.rimSize.startsWith('7.5j x 18'))"></v-divider>
 
+      <v-card-title v-if="rims.some(rim => rim.rimSize.startsWith('8.5j x 19'))">19"</v-card-title>
+      <v-row class="my-2" v-if="rims.some(rim => rim.rimSize.startsWith('8.5j x 19'))">
+        <v-col v-for="rim in rims.filter(rim => rim.rimSize.startsWith('8.5j x 19'))" :key="rim._id">
+          <rimsCardComponent
+            :rimName="rim.rimName"
+            :velicina="rim.rimSize"
+            :cijenaR="rim.priceR"
+            :image-url="rim.rimImageUrl"
+          />
+        </v-col>
+      </v-row>
+      <v-divider v-if="rims.some(rim => rim.rimSize.startsWith('8.5j x 19'))"></v-divider>
+
+      <v-card-title v-if="rims.some(rim => rim.rimSize.startsWith('9.0j x 20'))">20"</v-card-title>
+      <v-row class="my-2" v-if="rims.some(rim => rim.rimSize.startsWith('9.0j x 20'))">
+        <v-col v-for="rim in rims.filter(rim => rim.rimSize.startsWith('9.0j x 20'))" :key="rim._id">
+          <rimsCardComponent
+            :rimName="rim.rimName"
+            :velicina="rim.rimSize"
+            :cijenaR="rim.priceR"
+            :image-url="rim.rimImageUrl"
+          />
+        </v-col>
+      </v-row>
+    </template>
   </v-container>
 </template>
 
 <script>
-import rimsCardComponent from "@/components/rimsCardComponent.vue"
+import axios from 'axios';
+import rimsCardComponent from "@/components/rimsCardComponent.vue";
 
 export default {
   name: 'ConfigView1',
 
-  props: {
-    carAuto: String,
-    priceAuto: Number
-  },
-
   components: {
     rimsCardComponent
   },
-}
+
+  data() {
+    return {
+      rims: [],
+      loading: true,
+      error: null,
+    };
+  },
+
+  methods: {
+    async fetchRims() {
+      try {
+        const response = await axios.get('http://localhost:3000/rims');
+        this.rims = response.data;
+      } catch (error) {
+        this.error = 'Failed to load rims. Please try again later.';
+        console.error('Error fetching rims:', error);
+      } finally {
+        this.loading = false;
+      }
+    }
+  },
+
+  created() {
+    this.fetchRims();
+  }
+};
 </script>
+
+<style scoped>
+.loading {
+  color: orange;
+  font-size: 24px;
+}
+
+.error {
+  color: red;
+  font-size: 24px;
+}
+
+v-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
